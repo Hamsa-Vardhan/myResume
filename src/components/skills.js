73ca2b9c04a461e56aeb718c46f6c07b15228b40
Hamsa-Vardhan <hamsavardhan99@gmail.com>
2020-08-28 13:25:28 +0530

    changed skills

--- src/components/skills.js
+++ src/components/skills.js
@@ -3,123 +3,97 @@ import { AiFillHtml5 } from 'react-icons/ai';
 import { FaCss3Alt, FaNodeJs, FaReact, FaNode } from 'react-icons/fa';
 import { IoLogoPython, IoLogoAngular } from 'react-icons/io';
 import { GrMysql } from 'react-icons/gr';
-import { DiMongodb } from 'react-icons/di';
+import { AiOutlineAmazon } from 'react-icons/ai';
+import { DiMongodb, DiJavascript1, DiRedis, DiPostgresql, DiFirebase } from 'react-icons/di';
 function Skills() {
 	return (
 		<React.Fragment>
 			<div className="col-12">
-				<p className="container row">
-					<span>
-						<AiFillHtml5 />
-						<FaCss3Alt />HTML/CSS
-					</span>
-				</p>
-				<p className="container row">
-					<span className="col-10">
-						<span style={{ right: '10%' }} />
-					</span>{' '}
-					90%
-				</p>
+				<div className="container row skill-parent">
+					<h5 className="col-12"> Coding Languages .... </h5>
+					<ul className="col-12">
+						<li>
+							{' '}
+							<FaNodeJs /> Node JS
+						</li>
+						<li>Type Script</li>
+						<li>
+							{' '}
+							<IoLogoPython /> Python
+						</li>
+					</ul>
+				</div>
 			</div>
 			<div className="col-12">
-				<p className="container row">
-					<span>TYPE SCRIPT</span>
-				</p>
-				<p className="container row">
-					<span className="col-10">
-						<span />
-					</span>{' '}
-					80%
-				</p>
+				<div className="container row skill-parent">
+					<h5 className="col-12">Front End .... </h5>
+					<ul className="col-12">
+						<li>
+							{' '}
+							<AiFillHtml5 /> Html / <FaCss3Alt /> css / <DiJavascript1 /> JS
+						</li>
+						<li>
+							{' '}
+							<FaReact /> React
+						</li>
+						<li>
+							{' '}
+							<IoLogoAngular /> Angular
+						</li>
+					</ul>
+				</div>
 			</div>
 			<div className="col-12">
-				<p className="container row">
-					<span>
-						<IoLogoPython /> PYTHON{' '}
-					</span>
-				</p>
-				<p className="container row">
-					<span className="col-10">
-						<span style={{ right: '15%' }} />
-					</span>{' '}
-					85%
-				</p>
+				<div className="container row skill-parent">
+					<h5 className="col-12">Back End ....</h5>
+					<ul className="col-12">
+						<li> Express </li>
+						<li> Flask </li>
+					</ul>
+				</div>
 			</div>
 			<div className="col-12">
-				<p className="container row">
-					<span>
-						<IoLogoAngular /> ANGULAR
-					</span>
-				</p>
-				<p className="container row">
-					<span className="col-10">
-						<span style={{ right: '20%' }} />
-					</span>{' '}
-					80%
-				</p>
+				<div className="container row skill-parent">
+					<h5 className="col-12"> Database .... </h5>
+					<ul className="col-12">
+						<li>
+							{' '}
+							<DiMongodb /> MongoDB{' '}
+						</li>
+						<li>
+							{' '}
+							<DiPostgresql /> Postgres{' '}
+						</li>
+						<li>
+							{' '}
+							<DiRedis /> redis{' '}
+						</li>
+					</ul>
+				</div>
 			</div>
 			<div className="col-12">
-				<p className="container row">
-					<span>
-						<FaReact /> REACT
-					</span>
-				</p>
-				<p className="container row">
-					<span className="col-10">
-						<span style={{ right: '20%' }} />
-					</span>{' '}
-					80%
-				</p>
+				<div className="container row skill-parent">
+					<h5 className="col-12">Serverless ....</h5>
+					<ul className="col-12">
+						<li>
+							{' '}
+							<DiFirebase /> Cloud Functions{' '}
+						</li>
+						<li>
+							{' '}
+							<AiOutlineAmazon /> Lambda Functions{' '}
+						</li>
+					</ul>
+				</div>
 			</div>
 			<div className="col-12">
-				<p className="container row">
-					<span>
-						<GrMysql /> MYSQL
-					</span>
-				</p>
-				<p className="container row">
-					<span className="col-10">
-						<span style={{ right: '25%' }} />
-					</span>{' '}
-					75%
-				</p>
-			</div>
-			<div className="col-12">
-				<p className="container row">
-					<span>
-						<DiMongodb /> MONGODB
-					</span>
-				</p>
-				<p className="container row">
-					<span className="col-10">
-						<span style={{ right: '15%' }} />
-					</span>{' '}
-					85%
-				</p>
-			</div>
-			<div className="col-12">
-				<p className="container row">
-					<span>
-						<FaNode /> NODEJS(EXPRESS)
-					</span>
-				</p>
-				<p className="container row">
-					<span className="col-10">
-						<span style={{ right: '10%' }} />
-					</span>{' '}
-					90%
-				</p>
-			</div>
-			<div className="col-12">
-				<p className="container row">
-					<span>FLASK</span>
-				</p>
-				<p className="container row">
-					<span className="col-10">
-						<span style={{ right: '25%' }} />
-					</span>{' '}
-					75%
-				</p>
+				<div className="container row skill-parent">
+					<h5 className="col-12">API ....</h5>
+					<ul className="col-12">
+						<li> Rest - ( Express Js ) </li>
+						<li> Graphql - ( Apollo ) </li>
+					</ul>
+				</div>
 			</div>
 		</React.Fragment>
 	);
